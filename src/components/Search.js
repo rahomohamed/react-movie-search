c4df263d153c184ae032fc2db750134f3f01f88c
Raho Mohamed <r.mohamed@quadfi.com>
2021-01-25 21:50:04 -0500

    implemented ability to search on every key stroke

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,45 +1,52 @@
-import React, {useState} from 'react';
-import "../styles/styles.css"; 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../styles/styles.css";
 import MovieCard from "./movieCard.js";
 
+export default function Search() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
-export default function Search(){
-  
-const [query, setQuery] = useState('');
-const [movies, setMovies] = useState([]);
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=90825a4fcff70ff150e671b6d56c3070&language=en-US&query=${query}&page=1&include_adult=false`;
 
-  const searchMovies = async (e) => {
-  e.preventDefault();
-  console.log("submitting");
-       
- const url = `https://api.themoviedb.org/3/search/movie?api_key=90825a4fcff70ff150e671b6d56c3070&language=en-US&query=${query}&page=1&include_adult=false`;
- 
- try {
-  const res = await fetch(url);
-  const data  = await res.json();
-  console.log(data.results)
-  setMovies(data.results);
-}catch(err){
-  console.error(err);
-}
-}
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setMovies(res.data.results);
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [query]);
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
 
   return (
-<>
-    <form className="search" onSubmit={searchMovies}>
-        <label className="search-label" htmlFor="query">Movie Name</label>
-        <input className="input" type="text" name="query"
-            placeholder="i.e. Jurassic Park"
-            value={query} 
-            onChange={(e) => setQuery(e.target.value)}
-            />
-               <button className="button" type="submit">Search</button>
-    </form>
-    <div className="card-list">
-                {movies.filter(movie => movie.poster_path).map(movie => (
-                <MovieCard movie={movie} key={movie.id}/>
-                ))}
-            </div>     
-</>
-)
+    <>
+      <form className="search">
+        <label className="search-label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query}
+          onChange={handleChange}
+        />
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
 }
